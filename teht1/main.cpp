2d972337cb159c269fe677cbc5b6ff4dd415584f
darkkeri <107490783+darkkeri@users.noreply.github.com>
2024-01-14 07:13:42 +0200

    teht1 täydennetty ja valmis

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -4,14 +4,27 @@
 
 using namespace std;
 
+int game(int maxnum);
+
 int main(){
+    int maxnumber;
+    int arvamaara;
+    cout<<"Anna arvauspelille suurin mahdollinen arvo kokonaislukuna. Pienin mahdollinen arvottu arvo on aina 1:"<<endl;
+    cin>>maxnumber;
+
+    arvamaara = game(maxnumber);
+    cout<<"Arvausten maara: "<<arvamaara<<endl;
+}
+
+int game(int maxnum){
+    int arvausmaara = 0;
     int arvaus;
     int oikealuku;
     srand(time(NULL));
-    oikealuku = rand()% 21;
+    oikealuku = rand()% maxnum+1;
 
     do{
-        cout<<"Arvaa kokonaisluku 1-20: "<<endl;
+        cout<<"Arvaa kokonaisluku 1-"<<maxnum<<": "<<endl;
         cin>>arvaus;
 
         if(arvaus==oikealuku){
@@ -21,6 +34,7 @@ int main(){
         } else if(arvaus<oikealuku){
             cout<<"luku on suurempi"<<endl;
         }
+        arvausmaara++;
     } while (arvaus != oikealuku);
-    return 0;
+    return arvausmaara;
 }
